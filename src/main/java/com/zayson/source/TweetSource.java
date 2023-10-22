21c6332533a446cee142d52b4b75fbc78d88f949
zayson <jymaeng95@gmail.com>
2023-10-22 23:12:52 +0900

    feat: Tweet Source 생성

---
+++ src/main/java/com/zayson/source/TweetSource.java
@@ -0,0 +1,40 @@
+package com.zayson.source;
+
+import com.zayson.model.Tweet;
+import org.apache.flink.api.connector.source.util.ratelimit.RateLimiterStrategy;
+import org.apache.flink.api.java.typeutils.GenericTypeInfo;
+import org.apache.flink.connector.datagen.source.DataGeneratorSource;
+import org.apache.flink.connector.datagen.source.GeneratorFunction;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class TweetSource {
+    private static final List<String> sources = List.of("en", "kr", "jp");
+    private static final List<String> texts = List.of("bitcoin", "bitcoin ethereum", "ethereum");
+    private static final List<Boolean> tweetStatus = List.of(true, false);
+
+    private static final double RECORDS_PER_SECONDS = 3;
+    public static DataGeneratorSource<Tweet> createTweetSource() {
+        GeneratorFunction<Long, Tweet> tweetGenerator = index -> Tweet.builder()
+                .id(Instant.now().getEpochSecond())
+                .source(randomData(sources))
+                .text(randomData(texts))
+                .createdAt(Instant.now().getEpochSecond())
+                .modifiedAt(Instant.now().getEpochSecond())
+                .isRetweet(randomData(tweetStatus))
+                .build();
+
+        return new DataGeneratorSource<>(
+                tweetGenerator,
+                Long.MAX_VALUE,
+                RateLimiterStrategy.perSecond(RECORDS_PER_SECONDS),
+                GenericTypeInfo.of(Tweet.class)
+        );
+    }
+
+    private static <T> T randomData(List<T> targets) {
+        return targets.get(ThreadLocalRandom.current().nextInt(0, targets.size()));
+    }
+}
